5a7d6e13abf30a48a48e340320737fe50dd254ed
Samuel Rabinowitz <srabinowitz9@gmail.com>
2018-04-14 15:40:43 -0400

    beginning stuff

--- Bubble Popper/popup.js
+++ Bubble Popper/popup.js
@@ -11,5 +11,22 @@ document.addEventListener('DOMContentLoaded', function() {
 */
 document.addEventListener('DOMContentLoaded', function() {
   var frame = document.getElementById('mainFrame');
-  frame.src = "https://m.bing.com/"
+
+  var politicalBias = 0.12; // Political bias ranges from [-1.5, +1.5] where -1.5 is very liberal, 0.0 is neutral, and +1.5 is very conservative.
+
+  // Get the title of the article from the title of the tab
+  chrome.tabs.getSelected(null, function(tab) {
+    var articleTitle = tab.title;
+
+    // Read in the data copied from https://topbottomcenter.com/stats/ about AI-determined biases of news sources
+    var new File("/source_bias_data.csv");
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(event){
+      var csv = event.target.result;
+      var newsSources = $.csv.toObjects(csv);
+      console.log(newsSources);
+    }
+  });
+  frame.src = "";//"https://m.bing.com/"
 }, false);
